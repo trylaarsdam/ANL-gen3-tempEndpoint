042ed0ede26680a825efedb07a30f93a66fd87c0
Todd Rylaarsdam <todd@toddr.org>
2021-07-13 11:30:57 -0500

    filesystem

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ const express = require('express')
 const app = express()
 const port = 80
 app.use(express.json()) // for parsing application/json
+const fs = require('fs');
 
-var data = []
+
 
 app.get('/', (req, res) => {
   res.send('anl gen3 mote temp endpoint')
 })
 
 app.post('/api/v1/data', (req, res) => {
-  data.push(req.body)
+  fs.appendFile('log.json', JSON.stringify(req.body), function (err) {
+    if (err) throw err;
+    console.log('saved');
+  });
 })
 
 app.get('/api/v1/data', (req, res) => {
-  res.send(JSON.stringify(data))
+  fs.readFile('log.json', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  res.send(data);
+})
 })
 
 app.listen(port);
